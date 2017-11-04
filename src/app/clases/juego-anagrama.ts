0bca2292bb3b7c0a4a4c1188a0a7cbfc0fd9dae6
mauro <mauro.s@outlook.com>
2017-11-04 19:32:40 -0200

    anagrama logica

--- src/app/clases/juego-anagrama.ts
+++ src/app/clases/juego-anagrama.ts
@@ -1,2 +1,81 @@
-export class JuegoAnagrama {
+import { Juego } from '../clases/juego'
+export class JuegoAnagrama extends Juego {
+
+    
+    palabraOrdenada="";
+    palabraDesordenada="";
+    palabraIngresada="";
+    esperando=0;
+    
+    constructor(nombre?: string, gano?: boolean, jugador?:string) {
+        super("Anagrama",gano,jugador);
+    }
+     
+
+//aca hacer el array con palabras
+
+palabras : Array<any>=[
+    {
+        desordenada:"racngi",
+        ordenada:"racing"
+    },
+    {
+        desordenada:"rierv",
+        ordenada:"river"
+    },
+    {
+        desordenada:"cabo",
+        ordenada:"boca"
+    },
+    {
+        desordenada:"inpendediente",
+        ordenada:"independiente"
+    },
+    {
+        desordenada:"lorenzosan",
+        ordenada:"san lorenzo"
+    }
+];
+
+    public generarPalabra() {
+      /*  this.numeroSecreto = Math.floor((Math.random() * 100) + 1);
+       console.info('numero Secreto:' + this.numeroSecreto);
+        this.gano = false;
+        */
+        
+        let indice;
+        indice =Math.floor((Math.random() * 5) + 0);
+        console.log(this.palabras[indice]["ordenada"]);
+        this.palabraDesordenada=this.palabras[indice]["desordenada"];
+        this.palabraOrdenada=this.palabras[indice]["ordenada"];
+
+        this.esperando=1;
+      }
+
+      public verificar() {
+        if (this.palabraIngresada == this.palabraOrdenada ) 
+        {
+          this.gano = true;
+          this.palabraOrdenada="";
+          this.palabraDesordenada="";
+          this.palabraIngresada="";
+          this.esperando=0;
+        }
+        if (this.gano) {
+          return true;
+        } else {
+          return false;
+        }
+     }
+
+
+      public retornarAyuda() {
+       /* if (this.numeroIngresado < this.numeroSecreto) {
+          return "Falta";
+        }
+        return "Te pasate";
+        */
+        return "algo" ;
+      }
+
 }
--- src/app/componentes/anagrama/anagrama.component.ts
+++ src/app/componentes/anagrama/anagrama.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { JuegoAnagrama } from '../../clases/juego-anagrama'
 
 @Component({
   selector: 'app-anagrama',
@@ -7,7 +8,90 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AnagramaComponent implements OnInit {
 
-  constructor() { }
+  nuevoJuego: JuegoAnagrama;
+  Mensajes:string;
+  contador:number;
+  ocultarVerificar:boolean;
+  
+
+ 
+
+  constructor() {
+    this.nuevoJuego = new JuegoAnagrama();
+    //console.info("numero Secreto:",this.nuevoJuego.numeroSecreto);  
+    this.ocultarVerificar=true;
+   }
+
+   generarPalabra() {
+    this.nuevoJuego.generarPalabra();
+    this.contador=0;
+    this.ocultarVerificar=false;
+  }
+
+  verificar()
+  {
+    this.contador++;
+    this.ocultarVerificar=true;
+    //console.info("numero Secreto:",this.nuevoJuego.gano);  
+    if (this.nuevoJuego.verificar()){
+      
+     // this.enviarJuego.emit(this.nuevoJuego);
+      this.MostarMensaje("Sos un Genio!!!",true);
+  
+
+    }else{
+
+      let mensaje:string;
+      switch (this.contador) {
+        case 1:
+          mensaje="No, intento fallido, animo";
+          break;
+          case 2:
+          mensaje="No,Te estaras Acercando???";
+          break;
+          case 3:
+          mensaje="No es, Yo crei que la tercera era la vencida.";
+          break;
+          case 4:
+          //mensaje="No era el  "+this.nuevoJuego.numeroIngresado;
+          break;
+          case 5:
+          mensaje=" intentos y nada.";
+          break;
+          case 6:
+          mensaje="Afortunado en el amor";
+          break;
+      
+        default:
+            mensaje="Ya le erraste "+ this.contador+" veces";
+          break;
+      }
+      this.MostarMensaje("#"+this.contador+" "+mensaje+" ayuda :"+this.nuevoJuego.retornarAyuda());
+     
+
+    }
+    console.info("numero Secreto:",this.nuevoJuego.gano);  
+  }  
+
+  MostarMensaje(mensaje:string="este es el mensaje",ganador:boolean=false) {
+    this.Mensajes=mensaje;    
+    var x = document.getElementById("snackbar");
+    if(ganador)
+      {
+        x.className = "show Ganador";
+      }else{
+        x.className = "show Perdedor";
+      }
+    var modelo=this;
+    setTimeout(function(){ 
+      x.className = x.className.replace("show", "");
+      modelo.ocultarVerificar=false;
+     }, 3000);
+    console.info("objeto",x);
+  
+   }  
+
+
 
   ngOnInit() {
   }
