709dd2ed9530d5cf48fa13ff3d5d11abb5da0aa4
mauro <mauro.s@outlook.com>
2017-11-14 00:30:37 -0200

    Terminado agilidada. Cambio de letra

--- src/app/clases/juego-agilidad.ts
+++ src/app/clases/juego-agilidad.ts
@@ -1,2 +1,80 @@
-export class JuegoAgilidad {
+import { Juego } from '../clases/juego';
+
+export class JuegoAgilidad extends Juego {
+
+    numeroUno: number;
+    operador: string;
+    numeroDos: number;
+    arrayOperadores : Array<string> = ["+","-","*","/"];
+    numeroIngresado: number;
+    resultadoBien:number
+ 
+    constructor(nombre?: string, gano?: boolean, jugador?:string) {
+        super("Agilidad aritmética",gano,jugador);
+    }
+     
+    public generar() {
+          
+          let indiceNumero;
+          indiceNumero =Math.floor((Math.random() * 10) + 0);
+        this.numeroUno = indiceNumero;
+        indiceNumero =Math.floor((Math.random() * 10) + 0);
+        this.numeroDos= indiceNumero;
+
+        let indiceOper;
+        indiceOper =Math.floor((Math.random() * 4) + 0);
+        this.operador = this.arrayOperadores[indiceOper];
+
+        switch(this.operador)
+        {
+            case "+":
+                 this.resultadoBien = this.numeroUno + this.numeroDos;
+                 break;
+            case "-":
+                 this.resultadoBien = this.numeroUno - this.numeroDos;
+                 break;
+            case "*":
+                 this.resultadoBien = this.numeroUno * this.numeroDos;
+                 break;
+            case "/":
+                  if(this.numeroDos == 0)
+                  {
+                    indiceNumero =Math.floor((Math.random() * 10) + 0);
+                    this.numeroDos= indiceNumero;
+                      this.resultadoBien = this.numeroUno / this.numeroDos;
+                  }
+                  else
+                    {
+                        this.resultadoBien = this.numeroUno / this.numeroDos;
+                    }
+                break;
+        }
+        }
+  
+        public verificar() {
+          
+        if(this.resultadoBien== this.numeroIngresado)
+            {
+                this.gano=true;
+            }
+            else{
+                this.gano=false;
+            }
+          
+          if (this.gano) {
+            return true;
+          } else {
+            return false;
+          }
+       }
+  
+  
+        public retornarAyuda() {
+         /* if (this.numeroIngresado < this.numeroSecreto) {
+            return "Falta";
+          }
+          return "Te pasate";
+          */
+          return "algo" ;
+        }
 }
--- src/app/componentes/agilidad-aritmetica/agilidad-aritmetica.component.ts
+++ src/app/componentes/agilidad-aritmetica/agilidad-aritmetica.component.ts
@@ -15,17 +15,27 @@ export class AgilidadAritmeticaComponent implements OnInit {
   ocultarVerificar: boolean;
   Tiempo: number;
   repetidor:any;
+  Mensajes:string;
+
+  resultados  : Array<JuegoAgilidad> = new Array<JuegoAgilidad>();
+  
   private subscription: Subscription;
   ngOnInit() {
   }
    constructor() {
      this.ocultarVerificar=true;
-     this.Tiempo=5; 
+     this.Tiempo=8; 
     this.nuevoJuego = new JuegoAgilidad();
     console.info("Inicio agilidad");  
+    if(localStorage.getItem("resultados")!=null)
+      {
+    this.resultados = JSON.parse(localStorage.getItem("resultados"));
+      }
   }
   NuevoJuego() {
+    this.Tiempo=8;
     this.ocultarVerificar=false;
+    this.nuevoJuego.generar();
    this.repetidor = setInterval(()=>{ 
       
       this.Tiempo--;
@@ -34,7 +44,7 @@ export class AgilidadAritmeticaComponent implements OnInit {
         clearInterval(this.repetidor);
         this.verificar();
         this.ocultarVerificar=true;
-        this.Tiempo=5;
+        this.Tiempo=8;
       }
       }, 900);
 
@@ -43,9 +53,51 @@ export class AgilidadAritmeticaComponent implements OnInit {
   {
     this.ocultarVerificar=false;
     clearInterval(this.repetidor);
-   
 
-   
+    if (this.nuevoJuego.verificar()){
+      
+      
+      this.MostarMensaje("Sos un Genio!!!",true);
+
+
+      this.resultados.push(new JuegoAgilidad(this.nuevoJuego.nombre,this.nuevoJuego.gano,this.nuevoJuego.jugador));
+      console.log(this.resultados);
+      localStorage.setItem("resultados",JSON.stringify(this.resultados));
+
+    }else{
+
+      this.resultados.push(new JuegoAgilidad(this.nuevoJuego.nombre,this.nuevoJuego.gano,this.nuevoJuego.jugador));
+      console.log(this.resultados);
+      localStorage.setItem("resultados",JSON.stringify(this.resultados));
+      
+      this.MostarMensaje("Perdiste!!! El resultado era: "+this.nuevoJuego.resultadoBien,false);
+
+    }
+    
+    //this.Tiempo = 8;
+    this.nuevoJuego.numeroDos=0;
+    this.nuevoJuego.numeroUno=0;
+    this.nuevoJuego.numeroIngresado=0;
+    this.nuevoJuego.operador="";
   }  
+
+  MostarMensaje(mensaje:string="este es el mensaje",ganador:boolean=false) {
+    this.Mensajes=mensaje;    
+    var x = document.getElementById("snackbar");
+    if(ganador)
+      {
+        x.className = "show Ganador";
+      }else{
+        x.className = "show Perdedor";
+      }
+    var modelo=this;
+    setTimeout(function(){ 
+      x.className = x.className.replace("show", "");
+      modelo.ocultarVerificar= true;
+     }, 3000);
+    console.info("objeto",x);
+  
+   }
+
 
 }
